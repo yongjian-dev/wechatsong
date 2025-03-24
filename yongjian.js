14ac614c48db0bc8693a3670a629299b98530aa9
yongjian_mubai <57282084+yongjian-dev@users.noreply.github.com>
2025-03-24 19:12:44 +0800

    Update yongjian.js

--- yongjian.js
+++ yongjian.js
@@ -4,7 +4,9 @@
 
 
 [rewrite_local]
+http-response ^https://www.hhlqilongzhu.cn/.*$ script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg180.js, requires-body=true, timeout=60, tag=黄白
+http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg178.js, requires-body=true, timeout=60, tag=黄白
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/yongjian-dev/wechatsong/refs/heads/main/mubai.js, requires-body=true, timeout=60, tag=mikoto
 
 [MitM]
-hostname = api.dragonlongzhu.cn
+hostname = api.xingzhige.com , www.hhlqilongzhu.cn , api.dragonlongzhu.cn
