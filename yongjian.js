592922fd92923875144dea6f4d5844e556dfa946
yongjian_mubai <57282084+yongjian-dev@users.noreply.github.com>
2025-03-24 19:20:09 +0800

    Update yongjian.js

--- yongjian.js
+++ yongjian.js
@@ -4,8 +4,8 @@
 
 
 [rewrite_local]
-http-response ^https://www.hhlqilongzhu.cn/.*$ script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg180.js, requires-body=true, timeout=60, tag=黄白
-http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg178.js, requires-body=true, timeout=60, tag=黄白
+http-response ^https://www.hhlqilongzhu.cn/.*$ script-path=https://raw.githubusercontent.com/yongjian-dev/wechatsong/refs/heads/main/mubai_hb.js, requires-body=true, timeout=60, tag=黄白
+http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/yongjian-dev/wechatsong/refs/heads/main/mubai_hb.js, requires-body=true, timeout=60, tag=黄白
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/yongjian-dev/wechatsong/refs/heads/main/mubai.js, requires-body=true, timeout=60, tag=mikoto
 
 [MitM]
